499002fde251d070fcf9750533f3e4789c85f2a2
Dhanushri26 <ds20262005@gmail.com>
2024-04-15 11:33:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from "jwt-decode";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <link rel="icon" type="image/x-icon" href="slot1.ico" />
+    <center />
+    <center />
+      <div class="container">
+      <div>
+      <img src="C:\Users\ADMIN\Desktop\Project\project3\src\Bannari_Amman_Institute_of_Technology_logo.png" alt="Bannari_Amman_Institute_of_Technology_logo.png" />
+      </div>
+      <div class="index">
+        <div class="index-int">
+          Welcome Back!
+          <br />
+          Experience makes everything perfect
+        </div>
+      </div>
+      <center />
+  
+    <div className='google'>
+      <GoogleOAuthProvider clientId="413661991117-nrfs68o293a0fjhnpcs169jjf4kbvp0h.apps.googleusercontent.com">
+      <GoogleLogin
+       onSuccess={credentialResponse => {
+       const decoded = jwtDecode(credentialResponse.credential);
+       console.log(decoded);
+    
+     }}
+        onError={() => {
+        console.log('Login Failed');
+  }}
+/>
+     </GoogleOAuthProvider>
+    </div> 
+  </div>
+  </div>
   );
 }
 
